c6ea4033b420a0226adba93008db7037eac81b4b
inmadv02 <dominguez.vamar21@triana.salesianos.edu>
2022-03-09 12:05:44 +0100

    Commit final

--- miarma/src/app/components/user-item/user-item.component.ts
+++ miarma/src/app/components/user-item/user-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AdminUser } from 'src/app/interfaces/users/userDTO';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-user-item',
@@ -10,9 +11,12 @@ export class UserItemComponent implements OnInit {
 
   @Input() userInput!: AdminUser;
 
-  constructor() { }
+  constructor(private userService : UsersService) { }
 
   ngOnInit(): void {
   }
 
+  beAdmin(id : string){
+    this.userService.createAdmins(id).subscribe(() => {location.reload()});
+  }
 }
--- miarma/src/app/services/users.service.ts
+++ miarma/src/app/services/users.service.ts
@@ -3,11 +3,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { AdminUserResponse} from '../interfaces/users/userDTO';
+import { AdminUser} from '../interfaces/users/userDTO';
+
+let tk = localStorage.getItem('token');
 
 const API_BASE_URL = `${environment.apiBaseUrl}`;
 const headers = new HttpHeaders({
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${localStorage.getItem('token')}`
+    'Authorization': `Bearer ${tk}`
   });
 
 @Injectable({
@@ -18,6 +21,11 @@ export class UsersService {
   constructor(private http : HttpClient) { }
 
   getAdmins() : Observable<AdminUserResponse>{
+    console.log(tk);
     return this.http.get<AdminUserResponse>(`${API_BASE_URL}/users`, {headers: headers});
+  }
+
+  createAdmins(id : string) : Observable<AdminUser> {
+    return this.http.put<AdminUser>(`${API_BASE_URL}/be-admin/${id}`, {headers: headers});
   }
 }
